06bb6a95a22804a243a3cf8321dba0848666fc9a
anass zeroual <anasszeroual09@gmail.com>
2024-04-08 15:29:01 +0000

    termpo

--- src/components/home/AddData.js
+++ src/components/home/AddData.js
@@ -3,6 +3,7 @@ import c from "./AddData.module.css";
 import Select from "react-select";
 import api from "../../service/api";
 import { getlabelandvalue } from "../functions/newUtils";
+import NetworkNotify from "../UI/NetworkNotify";
 
 const customStyles = {
   control: (provided, state) => ({
@@ -79,7 +80,8 @@ const AddData = (p) => {
     type: "",
   });
   const [kpiListOwner, setKpiListOwner] = useState(["first"]);
-
+  const [err, setErr] = useState({ status: false, message: "" });
+  const [success, setSuccess] = useState({ status: false, message: "" });
   const callback = useCallback(async () => {
     try {
       const response = await fetch(`${api}/dpm/kpiNames?kpiName=${p.title}`, {
@@ -101,110 +103,133 @@ const AddData = (p) => {
     callback();
   }, [callback]);
 
-
-  const submitHandler=async e=>{
+  const submitHandler = async (e) => {
     e.preventDefault();
-    if(control==="ad"){
-        try {
-          
-            await fetch(`${api}/${p.title}`, {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-                // Authorization: `Bearer ${isLoged.token}`,
-              },
-              body: JSON.stringify(dataAdded),
-            });
-      
-            // const datar = await response.json();
-            // console.Console(datar);
-            // p.click(e, p.title);
-          } catch (error) {
-            console.error("Error:", error);
-          }
+    if (control === "ad") {
+      try {
+        await fetch(`${api}/${p.title}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            // Authorization: `Bearer ${isLoged.token}`,
+          },
+          body: JSON.stringify(dataAdded),
+        });
+
+        // const datar = await response.json();
+        // console.Console(datar);
+        // p.click(e, p.title);
+        setDataAdded({
+          date: p.dateChoosen,
+          real: 0,
+          target: 0,
+          name: "",
+          type: "",
+        });
+        setSuccess({
+            status: true,
+            message: "data has been successfully added.",
+          });
+      } catch (error) {
+        console.error("Error:", error);
+        setErr({
+            status: true,
+            message:
+              "Something has gone wrong, we were not able to save this action, please try it again. ",
+          });
+      }
     }
+  };
+  if (err.status || success.status) {
+    setTimeout(() => {
+      setErr({ status: false, message: "" });
+      setSuccess({ status: false, message: "" });
+    }, 2000);
   }
-
-
   return (
-    <div className={c["form-container"]}>
-      <ul className={c.underList}>
-        <li
-          style={
-            control === "acp"
-              ? { opacity: 1, borderBottom: "2px solid white" }
-              : {}
-          }
-          onClick={(e) => setControl("acp")}
-        >
-          add action plan
-        </li>
-        <li
-          style={
-            control === "ad"
-              ? { opacity: 1, borderBottom: "2px solid white" }
-              : {}
-          }
-          onClick={(e) => setControl("ad")}
-        >
-          add data
-        </li>
+    <React.Fragment>
+    {err.status && <NetworkNotify message={err.message} success={false} />}
+      {success.status && (
+        <NetworkNotify message={success.message} success={true} />
+      )}
+      <div className={c["form-container"]}>
+        <ul className={c.underList}>
+          <li
+            style={
+              control === "acp"
+                ? { opacity: 1, borderBottom: "2px solid white" }
+                : {}
+            }
+            onClick={(e) => setControl("acp")}
+          >
+            add action plan
+          </li>
+          <li
+            style={
+              control === "ad"
+                ? { opacity: 1, borderBottom: "2px solid white" }
+                : {}
+            }
+            onClick={(e) => setControl("ad")}
+          >
+            add data
+          </li>
 
-        <li
-          style={
-            control === "ap"
-              ? { opacity: 1, borderBottom: "2px solid white" }
-              : {}
-          }
-          onClick={(e) => setControl("ap")}
-        >
-          add pareto
-        </li>
-      </ul>
-      <form className={c.form} onSubmit={submitHandler}>
-        <div className={c.inputD}>
-          <h3>choose date:</h3>
-          <input type="date" required value={dataAdded.date} disabled />
-        </div>
-        {control === "ad" && (
-          <React.Fragment>
-            <div className={c["form-group"]} style={{ alignItems: "center" }}>
-              <div className={c.inputC}>
-                <h3>choose Kpi:</h3>
-                <Select
-                  options={[
-                    ...getlabelandvalue(kpiListOwner),
-                    { label: "create new kpi", value: "create new kpi" },
-                  ]}
-                  id="modality"
-                  inputId="modality"
-                  styles={customStyles}
-                  placeholder="select KPI"
-                  onChange={(e) =>
-                    setDataAdded((p) => ({ ...p, name: e.value }))
-                  }
-                />
+          <li
+            style={
+              control === "ap"
+                ? { opacity: 1, borderBottom: "2px solid white" }
+                : {}
+            }
+            onClick={(e) => setControl("ap")}
+          >
+            add pareto
+          </li>
+        </ul>
+        <form className={c.form} onSubmit={submitHandler}>
+          <div className={c.inputD}>
+            <h3>choose date:</h3>
+            <input type="date" required value={dataAdded.date} disabled />
+          </div>
+          {control === "ad" && (
+            <React.Fragment>
+              <div className={c["form-group"]} style={{ alignItems: "center" }}>
+                <div className={c.inputC}>
+                  <h3>choose Kpi:</h3>
+                  <Select
+                    options={[
+                      ...getlabelandvalue(kpiListOwner),
+                      { label: "create new kpi", value: "create new kpi" },
+                    ]}
+                    id="modality"
+                    inputId="modality"
+                    styles={customStyles}
+                    placeholder="select KPI"
+                    onChange={(e) =>
+                      setDataAdded((p) => ({ ...p, name: e.value }))
+                    }
+                  />
+                </div>
+                {dataAdded.name === "create new kpi" && (
+                  <React.Fragment>
+                    OR
+                    <div className={c.inputC}>
+                      <h3>new Kpi:</h3>
+                      <input
+                        type="text"
+                        placeholder="Enter NEw KPI"
+                        required
+                        onBlur={(e) =>
+                          setDataAdded((p) => ({ ...p, name: e.target.value }))
+                        }
+                      />
+                    </div>
+                  </React.Fragment>
+                )}
               </div>
-              {dataAdded.name === "create new kpi" && (
-                <React.Fragment>
-                  OR
-                  <div className={c.inputC}>
-                    <h3>new Kpi:</h3>
-                    <input
-                      type="text"
-                      placeholder="Enter NEw KPI"
-                      required
-                      onChange={(e) =>
-                        setDataAdded((p) => ({ ...p, name: e.target.value }))
-                      }
-                    />
-                  </div>
-                </React.Fragment>
-              )}
-            </div>
-            <div className={c.inputD}>
-              <h3>choose type:</h3>
-              <Select
+              <div className={c.inputD}>
+                <h3>choose type:</h3>
+                <Select
                   options={[
                     { label: "negative", value: "negative" },
                     { label: "positive", value: "positive" },
@@ -217,40 +242,41 @@ const AddData = (p) => {
                     setDataAdded((p) => ({ ...p, type: e.value }))
                   }
                 />
-            </div>
-            <div className={c["form-group"]}>
-              <div className={c.inputC}>
-                <h3>real data:</h3>
-                <input
-                  type="number"
-                  placeholder="Enter your real data"
-                  step="0.01"
-                  required
-                  onChange={(e) =>
-                    setDataAdded((p) => ({ ...p, real: +e.target.value }))
-                  }
-                />
               </div>
-              <div className={c.inputC}>
-                <h3>target data:</h3>
-                <input
-                  type="number"
-                  placeholder="Enter your target data"
-                  step="0.01"
-                  required
-                  onChange={(e) =>
-                    setDataAdded((p) => ({ ...p, target: +e.target.value }))
-                  }
-                />
+              <div className={c["form-group"]}>
+                <div className={c.inputC}>
+                  <h3>real data:</h3>
+                  <input
+                    type="number"
+                    placeholder="Enter your real data"
+                    step="0.01"
+                    required
+                    onChange={(e) =>
+                      setDataAdded((p) => ({ ...p, real: +e.target.value }))
+                    }
+                  />
+                </div>
+                <div className={c.inputC}>
+                  <h3>target data:</h3>
+                  <input
+                    type="number"
+                    placeholder="Enter your target data"
+                    step="0.01"
+                    required
+                    onChange={(e) =>
+                      setDataAdded((p) => ({ ...p, target: +e.target.value }))
+                    }
+                  />
+                </div>
               </div>
-            </div>
-          </React.Fragment>
-        )}
-        <button className={c["form-submit-btn"]} type="submit">
-          submit
-        </button>
-      </form>
-    </div>
+            </React.Fragment>
+          )}
+          <button className={c["form-submit-btn"]} type="submit">
+            submit
+          </button>
+        </form>
+      </div>
+    </React.Fragment>
   );
 };
 export default AddData;
