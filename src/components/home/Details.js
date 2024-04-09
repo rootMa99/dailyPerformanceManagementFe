f1ffe9444f0fcfe3114ccd81a2338c17cced341c
anass zeroual <anasszeroual09@gmail.com>
2024-04-09 14:20:45 +0000

    tepm

--- src/components/home/Details.js
+++ src/components/home/Details.js
@@ -133,8 +133,8 @@ const Details = (p) => {
       : bgcolor.push("rgb(88, 3, 3)")
   );
   const datac = {
-    // labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
-    labels: deliveryData.map((m) => m.day),
+    labels: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
+    // labels: deliveryData.map((m) => m.day),
     datasets: [
       {
         type: "line",
