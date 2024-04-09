f21671b19bff9ea6e775d861b04c1afbadd6d0f3
anass zeroual <anasszeroual09@gmail.com>
2024-04-09 12:53:08 +0000

    pecr

--- src/service/api.js
+++ src/service/api.js
@@ -1,5 +1,5 @@
 
-// const api="http://10.236.150.25:8081";
- const api="http://localhost:8081";
- 
+const api="http://10.236.150.25:8080";
+//  const api="http://localhost:8080";
+
 export default api;
