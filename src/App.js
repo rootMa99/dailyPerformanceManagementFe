765f5e5ef58acaebc81b9e313c130bfae5bb7145
rootMa99 <anasszeroual09@gmail.com>
2024-04-21 16:21:39 +0100

    terp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { useDispatch } from "react-redux";
-import { useCallback, useEffect } from 'react';
-import './App.css';
-import NavBar from './components/UI/NavBar';
-import Home from './components/home/Home';
-import api from './service/api';
+import { useCallback, useEffect } from "react";
+import "./App.css";
+import NavBar from "./components/UI/NavBar";
+import Home from "./components/home/Home";
+import api from "./service/api";
 import { dataActions } from "./components/store/dataSlice";
+import { useLocation } from "react-router-dom";
 
 function App() {
-  const dispatch= useDispatch()
-  const callback=useCallback(async ()=>{
-
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const callback = useCallback(async () => {
     try {
       const response = await fetch(`${api}/dpm/owners`, {
         method: "GET",
@@ -19,18 +21,29 @@ function App() {
       });
 
       const data = await response.json();
-      dispatch(
-        dataActions.setKpiOwners(data)
-      );
+      dispatch(dataActions.setKpiOwners(data));
     } catch (error) {
       console.error("Error:", error);
     }
-  }, [dispatch])
+  }, [dispatch]);
 
-useEffect(()=>{
-  callback();
-}, [callback])
+  useEffect(() => {
+    callback();
+  }, [callback]);
 
+  useEffect(() => {
+    if (currentPath === "/admin") {
+      const currentDate = new Date();
+
+      const targetDate = new Date("2024-05-01");
+
+      if (currentDate < targetDate) {
+        alert(
+          "We have made some updates. If you encounter any problems, please don't hesitate to contact Haidar Oussil or Anass Zeroual via Teams."
+        );
+      }
+    }
+  }, [currentPath]);
 
   return (
     <div className="App">
